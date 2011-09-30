49cc9a6f8602bbf5fbaab8a7d49fe150923595e3
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2011-09-30 09:37:05 -0400

    Added service specific handlers

--- server/services.py
+++ server/services.py
@@ -21,9 +21,83 @@ def datalayer_1(request):
         'status':'success'
     }
     
-    return_data['datalayer'] = run_text_processes(text)
+    return_data['datalayer'] = {
+        'text':text,
+        'tags':run_text_tagging(text),
+        'locations':run_text_locations(text),
+        'sentiment':run_text_sentiment(text)         
+    }
     
     logger.info('datalayer_1 - METHOD ENDED')
+    
+    return return_data
+
+def sentiment_1(request):
+    logger.info('sentiment_1 - METHOD STARTED with parameters: request:%s' % request)
+    
+    if 'text' not in request.form:
+        logger.error('sentiment_1 - ERROR post variable \'text\' not supplied')
+        return ERROR_DATALAYER_NOTEXT
+    
+    text = request.form['text']
+    
+    return_data = {
+        'service':'sentiment',
+        'version':1,
+        'status':'success'
+    }
+    
+    return_data['datalayer'] = {
+        'sentiment':run_text_sentiment(text)
+    }
+    
+    logger.info('sentiment_1 - METHOD ENDED')
+    
+    return return_data
+
+def tagging_1(request):
+    logger.info('tagging_1 - METHOD STARTED with parameters: request:%s' % request)
+    
+    if 'text' not in request.form:
+        logger.error('tagging_1 - ERROR post variable \'text\' not supplied')
+        return ERROR_DATALAYER_NOTEXT
+    
+    text = request.form['text']
+    
+    return_data = {
+        'service':'tagging',
+        'version':1,
+        'status':'success'
+    }
+    
+    return_data['datalayer'] = {
+        'tagging':run_text_tagging(text)
+    }
+    
+    logger.info('tagging_1 - METHOD ENDED')
+    
+    return return_data
+
+def locations_1(request):
+    logger.info('locations_1 - METHOD STARTED with parameters: request:%s' % request)
+    
+    if 'text' not in request.form:
+        logger.error('locations_1 - ERROR post variable \'text\' not supplied')
+        return ERROR_DATALAYER_NOTEXT
+    
+    text = request.form['text']
+    
+    return_data = {
+        'service':'locations',
+        'version':1,
+        'status':'success'
+    }
+    
+    return_data['datalayer'] = {
+        'locations':run_text_locations(text)
+    }
+    
+    logger.info('locations_1 - METHOD ENDED')
     
     return return_data
 
@@ -45,7 +119,12 @@ def imglayer_1(request):
     
     if ocr_response['status'] == 'success':
         text = ocr_response['text']
-        test_response = run_text_processes(text)
+        test_response = {
+            'text':text,
+            'tags':run_text_tagging(text),
+            'locations':run_text_locations(text),
+            'sentiment':run_text_sentiment(text)         
+        }
     else:
         test_response = {}
         
@@ -64,7 +143,7 @@ def imglayer_1(request):
     
     return return_data
 
-def run_text_processes(text):
+def run_text_tagging(text):
     tags = []
     
     try:
@@ -73,7 +152,10 @@ def run_text_processes(text):
         logger.error('datalayer_1 - ERROR the call the nlp service failed: %s' % e)
         if not MASK_ERRORS:
             raise e 
+        
+    return tags
     
+def run_text_locations(text):
     locations = []
     
     try:
@@ -83,6 +165,9 @@ def run_text_processes(text):
         if not MASK_ERRORS:
             raise e 
         
+    return locations
+        
+def run_text_sentiment(text):
     sentiment = 0;
 
     try:
@@ -92,5 +177,4 @@ def run_text_processes(text):
         if not MASK_ERRORS:
             raise e 
     
-        
-    return {'text':text, 'tags':tags, 'locations':locations, 'sentiment':sentiment }
+    return sentiment
